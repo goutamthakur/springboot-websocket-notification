4a819fc3955bedddaf39803fcd150bea115e8774
goutamthakur <goutamthakur2508@gmail.com>
2026-03-01 22:00:29 +0530

    feat: added heartbeat

--- src/main/java/com/goutamthakur/notification/config/WebSocketConfig.java
+++ src/main/java/com/goutamthakur/notification/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.goutamthakur.notification.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,8 +22,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(taskScheduler());
         registry.setApplicationDestinationPrefixes("/app");
 
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler(){
+        return new ThreadPoolTaskScheduler();
     }
 }
